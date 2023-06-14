0b25b9e5f686c59629927341ae1289b031024166
DNekrassov <nekrassov.daniel@gmail.com>
2023-06-15 00:27:32 +0300

    Changed status handling, adding status.json and status.py enum class

--- PrivacyHighlighter/routes.py
+++ PrivacyHighlighter/routes.py
@@ -4,14 +4,9 @@ from flask import Blueprint, request, jsonify, json
 from PrivacyHighlighter.db_models import Policy, DEFAULT_JSON
 from PrivacyHighlighter.utils import check_policy_by_url, check_result_json_validity
 from PrivacyHighlighter.chatgpt_integration import chatgpt_policy_request
+from PrivacyHighlighter.status import Status
 
 main = Blueprint('main', __name__)
-
-MSG_ERR_INVALID_ARG = "Invalid arguments passed to the system"
-MSG_ERR_INVALID_URL = "Invalid URL passed to the system"
-MSG_ERR_NO_POLICY_FOUND = "No Privacy Policy was found for this URL in our database"
-MSG_ERR_INVALID_RESULT = "Our system hasn't managed to produce a valid result for the Privacy Policy"
-MSG_SUCCESS = "Successful ChatGPT response"
 
 
 @main.route("/")
@@ -22,27 +17,27 @@ def default_route():
 @main.route("/request")
 def request_route():
     if 'policy_url' not in request.args:
-        return jsonify(response_type="Fail", message=MSG_ERR_INVALID_ARG, result_json=DEFAULT_JSON)
+        return jsonify(message=Status.MSG_ERR_INVALID_ARG, result_json=DEFAULT_JSON)
 
     policy_url = request.args.get('policy_url')
 
     if not validators.url(policy_url):
-        return jsonify(response_type="Fail", message=MSG_ERR_INVALID_URL, result_json=DEFAULT_JSON)
+        return jsonify(message=Status.MSG_ERR_INVALID_URL, result_json=DEFAULT_JSON)
 
     policy = check_policy_by_url(policy_url)
 
     if not policy or not policy.has_policy:
-        return jsonify(response_type="Fail", message=MSG_ERR_NO_POLICY_FOUND, result_json=DEFAULT_JSON)
+        return jsonify(message=Status.MSG_ERR_NO_POLICY_FOUND, result_json=DEFAULT_JSON)
 
     if policy.has_result:
-        return jsonify(response_type="Success", message=MSG_SUCCESS, result_json=policy.gpt_result)
+        return jsonify(message=Status.MSG_SUCCESS, result_json=policy.gpt_result)
 
     # else, parse result through ChatGPT
     gpt_result = chatgpt_policy_request(policy.policy_text)
 
     if not check_result_json_validity(gpt_result):
-        return jsonify(response_type="Fail", message=MSG_ERR_INVALID_RESULT, result_json=DEFAULT_JSON)
+        return jsonify(message=Status.MSG_ERR_INVALID_RESULT, result_json=DEFAULT_JSON)
 
     policy.update_gpt_result(gpt_result)
 
-    return jsonify(response_type="Success", message=MSG_SUCCESS, result_json=policy.gpt_result)
+    return jsonify(message=Status.MSG_SUCCESS, result_json=policy.gpt_result)
---
+++ PrivacyHighlighter/status.py
@@ -0,0 +1,9 @@
+from enum import Enum
+
+class Status(Enum):
+    MSG_SUCCESS = 0
+    MSG_ERR_INVALID_ARG = 1
+    MSG_ERR_INVALID_URL = 2
+    MSG_ERR_NO_POLICY_FOUND = 3
+    MSG_ERR_INVALID_RESULT = 4
+    MSG_IN_PROCESS = 5
